2f5e2983161a894dc4f29bb5dacf2502599167db
psanker <psanker@splunk.com>
2015-03-09 19:57:19 -0700

    finished problem 5

--- problem5.go
+++ problem5.go
@@ -4,6 +4,19 @@ import (
 )
 
 func main() {
-	
-	
+	for i := 20; ;i++ {
+		isEvenlyDivisible := true
+		for j:= 1; j < 21; j++ {
+			if i % j != 0 {
+				isEvenlyDivisible = false
+				break
+			}
+		}
+		if isEvenlyDivisible {
+			fmt.Println(i)
+			break
+		}
+
+	}
+
 }
